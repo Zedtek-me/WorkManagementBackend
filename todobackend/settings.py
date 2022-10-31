b337b4c1766815487f508b27dd09874c1964d98e
unknown <zechariahadebayo42@gmail.com>
2022-10-31 22:55:52 +0100

    updated the CORS headers for the recent port chosen by the react app.

--- todobackend/settings.py
+++ todobackend/settings.py
@@ -70,7 +70,7 @@ TEMPLATES = [
         },
     },
 ]
-CORS_ALLOWED_ORIGINS=["http://127.0.0.1:3000", "http://localhost:3000"]
+CORS_ALLOWED_ORIGINS=["http://localhost:*", "http://127.0.0.1:3001", "http://localhost:3001"]
 WSGI_APPLICATION = 'todobackend.wsgi.application'
 
 
